0bf8d1e07fd3dc9510f6a9fbe3ad8808620e7df8
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2016-02-23 16:00:24 +0100

    Make the build python scripts compatible with Python 3.x
    Builds have been tested with both Python 3.5 and 2.7 - 3.x builds
    still fail because of protobuf issues in external/nanopb-c, but the
    build scripts themselves are working with both interpreters.
    
    Change-Id: Iec45c120e069b9639661e99fd2500d8866418683
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- tools/event_log_tags.py
+++ tools/event_log_tags.py
@@ -14,6 +14,7 @@
 
 """A module for reading and parsing event-log-tags files."""
 
+from __future__ import print_function
 import re
 import sys
 
@@ -55,7 +56,7 @@ class TagFile(object):
     if file_object is None:
       try:
         file_object = open(filename, "rb")
-      except (IOError, OSError), e:
+      except (IOError, OSError) as e:
         self.AddError(str(e))
         return
 
@@ -64,6 +65,8 @@ class TagFile(object):
         self.linenum += 1
 
         line = line.strip()
+        if sys.version_info[0] >= 3:
+          line = str(line, "utf-8")
         if not line or line[0] == '#': continue
         parts = re.split(r"\s+", line, 2)
 
@@ -100,7 +103,7 @@ class TagFile(object):
 
         self.tags.append(Tag(tag, tagname, description,
                              self.filename, self.linenum))
-    except (IOError, OSError), e:
+    except (IOError, OSError) as e:
       self.AddError(str(e))
 
 
@@ -120,7 +123,7 @@ def WriteOutput(output_file, data):
   """Write 'data' to the given output filename (which may be None to
   indicate stdout).  Emit an error message and die on any failure.
   'data' may be a string or a StringIO object."""
-  if not isinstance(data, str):
+  if not isinstance(data, str) and not isinstance(data, bytes):
     data = data.getvalue()
   try:
     if output_file is None:
@@ -128,8 +131,14 @@ def WriteOutput(output_file, data):
       output_file = "<stdout>"
     else:
       out = open(output_file, "wb")
-    out.write(data)
+    if isinstance(data, bytes):
+      out.write(data)
+    else:
+      if sys.version_info[0] < 3:
+        out.write(str(data))
+      else:
+        out.write(str(data, "utf-8"))
     out.close()
-  except (IOError, OSError), e:
-    print >> sys.stderr, "failed to write %s: %s" % (output_file, e)
+  except (IOError, OSError) as e:
+    print("failed to write %s: %s" % (output_file, e), file=sys.stderr)
     sys.exit(1)
--- tools/java-event-log-tags.py
+++ tools/java-event-log-tags.py
@@ -23,7 +23,8 @@ tags in the given input file.
 -h to display this usage message and exit.
 """
 
-import cStringIO
+from __future__ import print_function
+from io import StringIO
 import getopt
 import os
 import os.path
@@ -34,26 +35,31 @@ import event_log_tags
 
 output_file = None
 
+if sys.version_info[0] >= 3:
+  # Py3 strings are already unicode, so...
+  def unicode(x):
+    return x
+
 try:
   opts, args = getopt.getopt(sys.argv[1:], "ho:")
-except getopt.GetoptError, err:
-  print str(err)
-  print __doc__
+except getopt.GetoptError as err:
+  print(str(err))
+  print(__doc__)
   sys.exit(2)
 
 for o, a in opts:
   if o == "-h":
-    print __doc__
+    print(__doc__)
     sys.exit(2)
   elif o == "-o":
     output_file = a
   else:
-    print >> sys.stderr, "unhandled option %s" % (o,)
+    print("unhandled option %s" % (o,), file=sys.stderr)
     sys.exit(1)
 
 if len(args) != 2:
-  print "need exactly two input files, not %d" % (len(args),)
-  print __doc__
+  print("need exactly two input files, not %d" % (len(args),))
+  print(__doc__)
   sys.exit(1)
 
 fn = args[0]
@@ -85,32 +91,32 @@ if "javadoc_hide" in tagfile.options:
 
 if tagfile.errors:
   for fn, ln, msg in tagfile.errors:
-    print >> sys.stderr, "%s:%d: error: %s" % (fn, ln, msg)
+    print("%s:%d: error: %s" % (fn, ln, msg), file=sys.stderr)
   sys.exit(1)
 
-buffer = cStringIO.StringIO()
+buffer = StringIO()
 buffer.write("/* This file is auto-generated.  DO NOT MODIFY.\n"
              " * Source file: %s\n"
-             " */\n\n" % (fn,))
+             " */\n\n" % (unicode(fn),))
 
-buffer.write("package %s;\n\n" % (tagfile.options["java_package"][0],))
+buffer.write("package %s;\n\n" % (unicode(tagfile.options["java_package"][0]),))
 
 basename, _ = os.path.splitext(os.path.basename(fn))
 
 if hide:
-  buffer.write("/**\n"
+  buffer.write(u"/**\n"
                " * @hide\n"
                " */\n")
-buffer.write("public class %s {\n" % (basename,))
-buffer.write("  private %s() { }  // don't instantiate\n" % (basename,))
+buffer.write(u"public class %s {\n" % (basename,))
+buffer.write(u"  private %s() { }  // don't instantiate\n" % (basename,))
 
 for t in tagfile.tags:
   if t.description:
-    buffer.write("\n  /** %d %s %s */\n" % (t.tagnum, t.tagname, t.description))
+    buffer.write(u"\n  /** %d %s %s */\n" % (t.tagnum, t.tagname, t.description))
   else:
-    buffer.write("\n  /** %d %s */\n" % (t.tagnum, t.tagname))
+    buffer.write(u"\n  /** %d %s */\n" % (t.tagnum, t.tagname))
 
-  buffer.write("  public static final int %s = %d;\n" %
+  buffer.write(u"  public static final int %s = %d;\n" %
                (t.tagname.upper(), t.tagnum))
 
 keywords = frozenset(["abstract", "continue", "for", "new", "switch", "assert",
@@ -138,15 +144,18 @@ for t in tagfile.tags:
     args = []
   argTypesNames = ", ".join([javaTypes[int(arg[1])] + " " + javaName(arg[0]) for arg in args])
   argNames = "".join([", " + javaName(arg[0]) for arg in args])
-  buffer.write("\n  public static void %s(%s) {" % (methodName, argTypesNames))
-  buffer.write("\n    android.util.EventLog.writeEvent(%s%s);" % (t.tagname.upper(), argNames))
-  buffer.write("\n  }\n")
+  buffer.write(u"\n  public static void %s(%s) {" % (methodName, argTypesNames))
+  buffer.write(u"\n    android.util.EventLog.writeEvent(%s%s);" % (t.tagname.upper(), argNames))
+  buffer.write(u"\n  }\n")
 
 
-buffer.write("}\n");
+buffer.write(u"}\n");
 
 output_dir = os.path.dirname(output_file)
 if not os.path.exists(output_dir):
   os.makedirs(output_dir)
+
+if sys.version_info[0] >= 3:
+  buffer=bytes(buffer.getvalue(), 'utf-8')
 
 event_log_tags.WriteOutput(output_file, buffer)
