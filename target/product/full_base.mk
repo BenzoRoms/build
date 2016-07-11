b869b8c402f3e100d86e945a02b51e762f7b0863
mydongistiny <jaysonedson@gmail.com>
2016-07-10 23:49:06 -0700

    NexusAudio: Fix no ringtone or notification sound
    Use Google Nexus Audio Files [1/2]
    3b3fcea5cce6c8d469290b69ff65ff2b0a203af8
    
    There was no default ringer or notification set
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -25,8 +25,8 @@ PRODUCT_PACKAGES := \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.config.ringtone=Luna.ogg \
+    ro.config.notification_sound=Tethys.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
